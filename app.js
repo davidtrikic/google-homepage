3961c211bde3fe675d80acccdde0d4f28ac111fa
davidtrikic <davidtrikic@outlook.com>
2021-06-22 20:32:56 +0200

    google apps menu script update

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const appsButton = document.getElementById('apps-button');
 const appsMenu = document.getElementById('apps-menu');
+const main = document.querySelector('main');
 
 appsButton.addEventListener('click', () => {
-if(window.getComputedStyle(appsMenu).display == 'none') appsMenu.style.display = 'block'
-    else appsMenu.style.display = 'none'
-})
+if(window.getComputedStyle(appsMenu).display == 'none') 
+    { 
+        appsMenu.style.display = 'block';
+        appsButton.style.backgroundColor = "#3c404314";
+    }
+    else { 
+        appsMenu.style.display = 'none';
+        appsButton.style.backgroundColor = "#fff";
+    }
+});
+
+main.addEventListener('click', () => {
+    if(window.getComputedStyle(appsMenu).display == 'block') 
+    {
+        appsMenu.style.display = 'none';
+        appsButton.style.backgroundColor = "#fff";
+    }
+});
