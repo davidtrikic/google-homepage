a55e1605f56b2f8dc676c7a6b87e3a9c095d3dc2
davidtrikic <davidtrikic@outlook.com>
2021-06-23 17:23:57 +0200

    add footer settings menu logic

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const appsButton = document.getElementById('apps-button');
+const settingsButton = document.getElementById('settings-button');
 const appsMenu = document.getElementById('apps-menu');
+const settingsMenu = document.getElementById('settings-menu');
 const main = document.querySelector('main');
 
+// Apps button
 appsButton.addEventListener('click', () => {
-if(window.getComputedStyle(appsMenu).display == 'none') 
+if (window.getComputedStyle(appsMenu).display == 'none') 
     { 
         appsMenu.style.display = 'block';
         appsButton.style.backgroundColor = "#3c404314";
@@ -14,10 +17,20 @@ if(window.getComputedStyle(appsMenu).display == 'none')
     }
 });
 
+// Settings button
+settingsButton.addEventListener('click', () => {
+    if (window.getComputedStyle(settingsMenu).display == 'none')    
+        settingsMenu.style.display = 'block';
+    else    settingsMenu.style.display = 'none';
+});
+
+// Remove menus if clicked outside of menu area
 main.addEventListener('click', () => {
     if(window.getComputedStyle(appsMenu).display == 'block') 
     {
         appsMenu.style.display = 'none';
         appsButton.style.backgroundColor = "#fff";
     }
+    if (window.getComputedStyle(settingsMenu).display == 'block') 
+        settingsMenu.style.display = 'none';
 });
